5be1bc96eda8791915e83097032419024d1a8743
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-06-10 17:14:35 -0500

    Updated File

---
+++ blackjack.py
@@ -0,0 +1,44 @@
+# count players
+# select deck count
+# store 1 deck of cards into data structure
+
+
+
+def countPlayers():
+    print("\nWelcome to Blackjack.")
+    totalPlayers = int(input("How many players?\n--> "))
+    return totalPlayers
+
+def selectDeckCount():
+    print("\nSelect total # of decks in game.")
+    totalDecks = int(input("How many decks?\n--> "))
+    return totalDecks
+    
+
+countPlayers()
+selectDeckCount()
+
+
+# class Dealer:
+#     def __init__(self):
+#         pass
+#     def newMethod(self):
+#         print("newMethod() in class Dealer")
+
+# class Player:
+#     def __init__(self):
+#         pass
+#     def newMethod(self):
+#         pass
+
+# class Game:
+#     def __init__(self):
+#         pass
+#     def newMethod(self):
+#         pass
+
+
+# d = Dealer()
+# p = Player()
+# g = Game()
+
