d55b4ea6896c54dd9be8308707bcd657816c0df3
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-06-10 17:07:18 -0500

    Updated File

---
+++ src/blackjack.py
@@ -0,0 +1,32 @@
+# count players
+# select deck count
+# store 1 deck of cards into data structure
+
+
+
+
+
+
+class Dealer:
+    def __init__(self):
+        pass
+    def newMethod(self):
+        print("newMethod() in class Dealer")
+
+class Player:
+    def __init__(self):
+        pass
+    def newMethod(self):
+        pass
+
+class Game:
+    def __init__(self):
+        pass
+    def newMethod(self):
+        pass
+
+
+d = Dealer()
+p = Player()
+g = Game()
+
